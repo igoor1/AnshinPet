f6b8869583e1c3224aefd0c052c00548f166264b
igoor1 <iggoncalves1@outlook.com>
2024-08-25 19:36:54 -0300

    first commit

---
+++ src/main/java/fatec/sp/gov/br/anshinpet/AnshinpetApplication.java
@@ -0,0 +1,13 @@
+package fatec.sp.gov.br.anshinpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnshinpetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnshinpetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fatec/sp/gov/br/anshinpet/AnshinpetApplicationTests.java
@@ -0,0 +1,13 @@
+package fatec.sp.gov.br.anshinpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnshinpetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
